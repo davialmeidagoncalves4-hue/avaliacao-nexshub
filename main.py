25f1e378c4391039394975a5dcd91ca1e6d560c6
davialmeidagoncalves4-hue <davialmeidagoncalves4@gmail.com>
2026-09-14 13:00:29 +0000

    prova1

---
+++ main.py
@@ -0,0 +1,12 @@
+#fase1
+sturtups ={
+    "nome":"cyberpulse tech"
+    "segmento": "segurança da informaçao"
+    "ano_deadesao":"2026"
+
+}
+solucoes_ativos =["Firewall IA", "scan de vunerabilidades"]
+
+print("startups", sturtups["nome"],)
+print("segmento",[0])
+
